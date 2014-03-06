f0ca586a00154a12b291615d2fb50ac8815d241d
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-06 17:57:39 -0300

    exibe apenas tarefas ativas

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -1,5 +1,10 @@
 class TarefasController < ApplicationController
   def index
     @tarefas = Tarefa.all
-	end
+  end
+
+  def ativas
+    @tarefas = Tarefa.where concluida: false
+    render :index
+    end
 end
