7b0bea26996bb214f7c32fc9c85ba8aedf6a4d0d
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-14 08:57:28 -0300

    corrige redirect e add before action

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -1,25 +1,24 @@
 class TarefasController < ApplicationController
+  before_action :count_all, only: [:index, :ativas, :concluidas, :edit]
+
   def index
     @tarefas = Tarefa.all
-    count_all
   end
 
   def ativas
     @tarefas = Tarefa.where concluida: false
-    count_all
     render :index
     end
 
   def concluidas
     @tarefas = Tarefa.where concluida: true
-    count_all
     render :index
   end
 
   def destroy
     tarefa = Tarefa.find params[:id]
     tarefa.destroy
-    redirect_to tarefas_path
+    redirect_to :back
   end
 
   def destroy_concluidas
@@ -29,24 +28,23 @@ class TarefasController < ApplicationController
 
   def toogle
     Tarefa.find(params[:id]).toggle! :concluida
-    redirect_to tarefas_path
+    redirect_to :back
     end
 
   def create
     Tarefa.create titulo: params[:tarefa], concluida: false
-    redirect_to tarefas_path
+    redirect_to :back
   end
 
   def edit
     @tarefa = Tarefa.find params[:id]
-    @tarefas = Tarefa.all
-    count_all
+    @tarefas = get_tarefas_edit
     render :index
   end
 
   def update
     Tarefa.update params[:id], titulo: params[:titulo]
-    redirect_to tarefas_path
+    redirect
   end
 
 private
@@ -69,4 +67,19 @@ private
     @count_totais = Tarefa.all.count
   end
 
+  def redirect
+    redirect_to case params[:list]
+      when 'concluidas' then concluidas_tarefas_path
+      when 'ativas' then ativas_tarefas_path
+      else tarefas_path
+    end
+  end
+
+  def get_tarefas_edit
+    case params[:list]
+      when 'concluidas' then Tarefa.concluidas
+      when 'ativas' then Tarefa.ativas
+      else Tarefa.all
+    end
+  end
 end
